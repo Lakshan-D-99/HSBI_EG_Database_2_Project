fc033609ad8e6bd584d0de50dd73ccc7b1a72b5f
Dhanuka <lakshandhanuka398@gmail.com>
2025-06-19 13:45:27 +0200

    Updated WebConfig to use the Url of the Frontend Application.

--- src/main/java/com/sp/hsbiegapi/config/WebConfig.java
+++ src/main/java/com/sp/hsbiegapi/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api_v1/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:9002")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
