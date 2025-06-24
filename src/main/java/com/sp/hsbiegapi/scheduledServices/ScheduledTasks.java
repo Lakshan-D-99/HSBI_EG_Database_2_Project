ab98de56e57f612574127ad63e2e370da309c9e4
Dhanuka <lakshandhanuka398@gmail.com>
2025-06-24 15:07:42 +0200

    Added a Scheduler to run every Day at 23:45 to get and store the Weather Data of a specific Location.

---
+++ src/main/java/com/sp/hsbiegapi/scheduledServices/ScheduledTasks.java
@@ -0,0 +1,46 @@
+package com.sp.hsbiegapi.scheduledServices;
+
+import com.sp.hsbiegapi.apiServices.weatherApi.WeatherApiService;
+import com.sp.hsbiegapi.models.locModels.Location;
+import com.sp.hsbiegapi.repositories.locatiRepositories.LocationRepository;
+import com.sp.hsbiegapi.services.locatiServices.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ScheduledTasks {
+
+    private final LocationRepository locationRepository;
+    private final WeatherService weatherService;
+
+    @Autowired
+    public ScheduledTasks(LocationRepository locationRepository, WeatherService weatherService, WeatherApiService weatherApiService) {
+        this.locationRepository = locationRepository;
+        this.weatherService = weatherService;
+    }
+
+    // Generate Weather Data every day for a specific Location
+    @Scheduled(cron = "0 45 23 * * *")
+    public void scheduleWeatherData(){
+
+        // Get all the Locations from the Database
+        List<Location> locationList = locationRepository.findAll();
+
+        // Loop through all the Locations and get the Longitude and the Latitude of a specific Location
+        for (Location location: locationList){
+
+            String longitude = location.getLocLongitude();
+            String latitude = location.getLocLatitude();
+
+            if (latitude.isEmpty() && longitude.isEmpty()){
+                throw new RuntimeException("Latitude and Longitude are missing");
+            }
+
+            weatherService.addWeatherData(location.getId(), longitude, latitude);
+
+        }
+    }
+}
